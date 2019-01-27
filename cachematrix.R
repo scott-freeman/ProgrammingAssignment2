165a490b0ec4a7adaab74800b05b97ffa8e93e95
scott-freeman <scott-freeman@users.noreply.github.com>
2019-01-27 14:52:41 -0800

    Update cachematrix.R with own code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Together, these functions cache the potentially time-consuming computation of calculating the
+## inverse of a square matrix. Therefore, if the inverse of the matrix has already been calculated,
+## it returns the transformed matrix instead of re-calculating the inverse.
 
-## Write a short comment describing this function
+# This function takes in a square matrix and creates a list containing functions to:
+# 1) set the value of the matrix, 2) get the value of the matrix, 3) set the value of the matrix, and
+# 4) get the value of the matrix. This allows computations that have already been caculated to
+# be cached, thus reducing computational time.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix = function(mat = matrix()) {
+  if (!is.matrix(mat) || ncol(mat) != nrow(mat)) {
+    message("Please enter a square matrix")
+  }
+  inv = NULL
+  set = function(y) {
+    mat <<- y
+    inv <<- NULL
+  }
+  get = function() mat
+  setinv = function(inverse) inv <<- inverse
+  getinv = function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
+# This function first checks to see if the inverse of the matrix has already been calculated
+# and stored in the makeCacheMatrix function. If it has, then the value is returned.
+# Otherwise, the inverse of the square matrix is calculated, returned, and cached for future use.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve = function(mat, ...) {
+  inv = mat$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data = mat$get()
+  inv = solve(data, ...)
+  mat$setinv(inv)
+  inv
 }
